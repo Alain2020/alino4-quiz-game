d71f8f506c744b58760e83969903e49df0ce4b86
Alain2020 <favouralain@yahoo.com>
2023-02-10 11:03:33 +0000

    Add score section to the quiz

--- run.py
+++ run.py
@@ -27,3 +27,9 @@ guesses = []
 score = 0
 
 question_num = 0 
+
+for question in questions:
+    print("-------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
