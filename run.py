685759278df6eb4f2c8e81283de9e7522834a56a
Alain2020 <favouralain@yahoo.com>
2023-02-11 01:30:38 +0000

    defining functions

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-print("Wellcome to quiz game !!")
-print('NOTE: All valid answers most be in (A,B,C,D) if not, then it is considered as wrong answer')
-score = 0
-question_no = 0
-playing = input('Do you want to play ? ').upper()
-if playing == 'yes':
-    question_no += 1
+def new_game():
+    pass
+def check_answer():
+    pass
+def display_score():
+    pass
+def play_again():
+    pass
