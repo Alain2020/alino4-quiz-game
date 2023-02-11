b5e2f658af09af53401c30895d6841bb8533ef93
Alain2020 <favouralain@yahoo.com>
2023-02-11 02:10:05 +0000

    Add questions dictionary with correct answer

--- run.py
+++ run.py
@@ -11,3 +11,15 @@ def display_score():
 def play_again():
     pass
 #------------------------------
+
+questions = {"What’s the fastest land animal in the world?: ": "A",
+             "How many hearts does an octopus have?: ": "C",,
+             "What country won the very first FIFA World Cup in 1930?: ": "D",
+             "What color is the tongue of a giraffe?: ": "B",
+             "For which team did Michael Jordan spend most of his career playing?: " : "D",
+             "Which country has the most Olympic gold medals in swimming?: " : "B",
+             "What year did boxing become a legal sport?: " : "A",
+             "Who was Prime Minister of Great Britain from 1841 to 1846?: " : "B",
+             "Name the East African country which lies on the Equator?: " : "A",
+             "Which country is the island of Zanzibar part of?: " : "C"
+             }
