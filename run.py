1fe1fabeed45e4c80b1996c3e110b412a50981f8
Alain2020 <favouralain@yahoo.com>
2023-02-11 02:19:12 +0000

    Add more options to the answer section

--- run.py
+++ run.py
@@ -23,3 +23,14 @@ questions = {"What’s the fastest land animal in the world?: ": "A",
              "Name the East African country which lies on the Equator?: " : "A",
              "Which country is the island of Zanzibar part of?: " : "C"
              }
+
+options   = [["A. The cheetah", "B. Lion", "C. Kangaroo", "D. Hyena "],
+            ["A. One", "B. Nine", "C.Three", "D. Five "],
+            ["A. Brazil", "B. United States", "C. Spain", "D.Uruguay "],
+            ["A. Black", "B. Purple", "C. Pink", "D. Grey "],
+            ["A. New York Knicks", "B. Orlando Magic", "C. Washington Wizards", "D. Chicago Bulls "],
+            ["A. China", "B. The USA", "C. The UK", "D. Australia "],
+            ["A. 1901", "B. 1921", "C. 1931", "D. 1911 "],
+            ["A. Anthony Eden", "B. Robert Peel", "C. Bonar Law", "D. Gordon Brown "],
+            ["A. Kenya", "B. Ethiopia", "C. Comoros", "D. Uganda "],
+            ["A. Mozambique", "B. Namibia", "C. Tanzania", "D. Botswana "]]
